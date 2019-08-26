dde72d215e44262f13c291a6344db87276efe1dd
NavyaZaveri <navyajayesh+1@gmail.com>
2019-08-26 18:35:23 +0100

    add test for /search endpoint

--- tests/test_server.py
+++ tests/test_server.py
@@ -9,8 +9,22 @@ def test_unauthorized_doc_insertion(server, xkcd_as_json):
 def test_authorized_doc_insertion(server, mock_xkcd):
     _, response = server.post("/insert", json={
         "doc": mock_xkcd.to_dict(),
-        "password": os.environ.get("PASSWORD")
+        "password": os.environ["PASSWORD"]
     })
     server.app.config.ES_CLIENT.refresh()
     assert len(server.app.config.ES_CLIENT.search_all().results()) == 1
     assert response.status_code == 201
+
+
+def test_expected_json_format(server, mock_xkcd):
+    _, response = server.post("/insert", json={
+        "doc": mock_xkcd.to_dict(),
+        "password": os.environ["PASSWORD"]
+    })
+    assert response.status_code == 201
+    _, response = server.get("/search", params={
+        "query": mock_xkcd.content
+    })
+    assert response.status_code == 200
+    assert "results" in response.json
+    assert len(response.json["results"]) == 1
